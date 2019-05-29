8b45882f250d81968b51c08279255b192056ecbe
Mu5tank05 <nathan@blaney.xyz>
2019-05-29 23:13:29 +1000

    Handle Lifecycle changes

--- app/src/main/java/au/edu/federation/capstoneprototype/MainActivity.java
+++ app/src/main/java/au/edu/federation/capstoneprototype/MainActivity.java
@@ -1,41 +1,49 @@
 package au.edu.federation.capstoneprototype;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.Manifest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Beacon> savedDevices;
-    ArrayList<Beacon> visibleDevices;
-    public static int REQUEST_BLUETOOTH = 1;
     BluetoothAdapter btAdapter;
-    public List<Beacon> list_beacons= new ArrayList<>();
+    public List<Beacon> list_beacons = new ArrayList<>();
     ListView saved;
     BeaconAdapter adapter;
     boolean searching = false;
+    int REQUEST_ENABLE_BT = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            // Device does not support Bluetooth
+        }
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter);
-
         adapter = new BeaconAdapter(this, list_beacons, false);
         saved = findViewById(R.id.saved_dynamic);
         saved.setAdapter(adapter);
@@ -43,28 +51,28 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(getPackageName(), "Searching...");
-                if(!searching){
+                if (!searching) {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+                    Log.d(getPackageName(), "Searching...");
                     btAdapter.startDiscovery();
                     searching = true;
                 }
-
             }
         });
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
         unregisterReceiver(mReceiver);
     }
+
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                list_beacons.add( new Beacon(device.getName(), device.getAddress(), String.valueOf(System.currentTimeMillis() / 1000L)));
+                list_beacons.add(new Beacon(device.getName(), device.getAddress(), String.valueOf(System.currentTimeMillis() / 1000L)));
                 Log.i("BT", device.getName() + "\n" + device.getAddress());
                 adapter.notifyDataSetChanged();
             }
