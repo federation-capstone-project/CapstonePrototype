1e977df11450d63a76acde4a32aa2d024eb95d32
Mu5tank05 <nathan@blaney.xyz>
2019-08-05 21:29:32 +1000

    Demo there (kinda) Need's DB cleaned

--- app/src/main/java/au/edu/federation/capstoneprototype/ClassFragment.java
+++ app/src/main/java/au/edu/federation/capstoneprototype/ClassFragment.java
@@ -24,21 +24,59 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
 public class ClassFragment extends Fragment {
-    BluetoothAdapter btAdapter;
-    ClassAdapter adapter;
     public List<Class> list_known = new ArrayList<>();
     public List<Class> list_classes = new ArrayList<>();
+    BluetoothAdapter btAdapter;
+    ClassAdapter adapter;
     ListView saved;
     boolean searching = false;
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                for (Class known : list_known) {
+                    if (known.getMacAddress().equals(device.getAddress())) {
+                        list_classes.add(known);
+                    }
+                }
+
+                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                adapter.notifyDataSetChanged();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Log.d(getActivity().getPackageName(), "Discovery Started");
+                Toast.makeText(getContext(), "Searching...", Toast.LENGTH_SHORT).show();
+                searching = true;
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.d(getActivity().getPackageName(), "Discovery Finished");
+                Toast.makeText(getContext(), "Searching Finished", Toast.LENGTH_SHORT).show();
+                searching = false;
+            }
+        }
+    };
     int REQUEST_ENABLE_BT = 0;
     SharedPreferences prefs;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        prefs = getActivity().getSharedPreferences("prefs",Context.MODE_PRIVATE);
+        prefs = getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE);
         return inflater.inflate(R.layout.fragment_class, container, false);
     }
 
@@ -109,6 +147,8 @@ public class ClassFragment extends Fragment {
                                     ImageView classStar = getView().findViewById(R.id.class_star);
                                     classStar.setVisibility(View.VISIBLE);
                                     current_class.setPresent(true);
+                                    postRequest(prefs.getString("student_id", "69"),String.valueOf(current_class.getClassId()), true, false);
+                                    Log.d(getActivity().getPackageName(), "Passed on to postRequest");
                                 }
                             })
 
@@ -128,9 +168,10 @@ public class ClassFragment extends Fragment {
                 LayoutInflater inflater = getActivity().getLayoutInflater();
                 View dialogView = inflater.inflate(R.layout.dialog_class_add, null);
 
-                final EditText editText = (EditText) dialogView.findViewById(R.id.edt_comment);
-                Button button1 = (Button) dialogView.findViewById(R.id.buttonSubmit);
-                Button button2 = (Button) dialogView.findViewById(R.id.buttonCancel);
+                final EditText classID =  dialogView.findViewById(R.id.et_class_id);
+                final EditText studentID =  dialogView.findViewById(R.id.et_student_id);
+                Button button1 =  dialogView.findViewById(R.id.buttonSubmit);
+                Button button2 =  dialogView.findViewById(R.id.buttonCancel);
 
                 button2.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -141,8 +182,9 @@ public class ClassFragment extends Fragment {
                 button1.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        // DO SOMETHINGS
                         dialogBuilder.dismiss();
+                        postRequest(studentID.getText().toString(), classID.getText().toString(), true, true);
+                        Toast.makeText(getContext(), "Class Successfully Checked In", Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -155,8 +197,8 @@ public class ClassFragment extends Fragment {
 
     }
 
-    public void deviceDiscovery(){
-        if(prefs.getBoolean("bluetooth_auto", true)){
+    public void deviceDiscovery() {
+        if (prefs.getBoolean("bluetooth_auto", true)) {
             if (!searching) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
                 Log.d(getActivity().getPackageName(), "Searching...");
@@ -167,29 +209,49 @@ public class ClassFragment extends Fragment {
 
     }
 
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                for (Class known : list_known) {
-                    if (known.getMacAddress().equals(device.getAddress())) {
-                        list_classes.add(known);
-                    }
+        public void postRequest(String student,  String event,  Boolean attended,  Boolean manual) {
+
+            MediaType MEDIA_TYPE = MediaType.parse("application/json");
+            String url = "https://capstone.blny.me/studentevents/";
+
+            OkHttpClient client = new OkHttpClient();
+
+            JSONObject postdata = new JSONObject();
+            try {
+                postdata.put("student", student);
+                postdata.put("event", event);
+                postdata.put("attended", attended);
+                postdata.put("manual", manual);
+            } catch(JSONException e){
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            RequestBody body = RequestBody.create(MEDIA_TYPE, postdata.toString());
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Token f89c13e25cd9d21a9d99d061d8e05e1cf5cff569")
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    String mMessage = e.getMessage();
+                    Log.d(getActivity().getPackageName(), mMessage);
+                    Log.d(getActivity().getPackageName(), "OnFailure");
+                    //call.cancel();
                 }
 
-                Log.i("BT", device.getName() + "\n" + device.getAddress());
-                adapter.notifyDataSetChanged();
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                Log.d(getActivity().getPackageName(), "Discovery Started");
-                Toast.makeText(getContext(), "Searching...", Toast.LENGTH_SHORT).show();
-                searching = true;
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                Log.d(getActivity().getPackageName(), "Discovery Finished");
-                Toast.makeText(getContext(), "Searching Finished", Toast.LENGTH_SHORT).show();
-                searching = false;
-            }
-        }
-    };
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
 
+                    String mMessage = response.body().string();
+                    Log.d(getActivity().getPackageName(), mMessage);
+                    Log.d(getActivity().getPackageName(), "OnResponse");
+                }
+            });
+        }
 }
