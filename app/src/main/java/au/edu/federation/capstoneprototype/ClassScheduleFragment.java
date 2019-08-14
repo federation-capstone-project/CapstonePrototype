3b4cea39edcac2f64299cb7818c126193a1f71a0
Mu5tank05 <nathan@blaney.xyz>
2019-08-14 17:12:11 +1000

    Move Fragment code around, clears error about schedule list Implement Room dependency for SQL

--- app/src/main/java/au/edu/federation/capstoneprototype/ClassScheduleFragment.java
+++ app/src/main/java/au/edu/federation/capstoneprototype/ClassScheduleFragment.java
@@ -2,10 +2,10 @@ package au.edu.federation.capstoneprototype;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,33 +14,36 @@ import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 
 public class ClassScheduleFragment extends Fragment {
 
     private TextView dateview;
-public LocalDateTime s;
+    public LocalDateTime s;
     private Context _context;
     private Button openDatePickerButton;
-    ListView list;
     ArrayList<CalItem> arrayList;
     CalendarList adapter;
     CalItem newCal;
 
 
     public ArrayList<CalItem> sss;
+    SharedPreferences prefs;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_class_schedule, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        prefs = getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+    // TODO Proper variable names @lcopsey
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         arrayList = new ArrayList<CalItem>();
-        adapter = new CalendarList(getContext(),arrayList);
-        ListView a = view.findViewById(R.id.lol);
+        adapter = new CalendarList(getContext(), arrayList);
+        ListView a = getView().findViewById(R.id.cal_dynamic);
         a.setAdapter(adapter);
         newCal = null;
         sss = new ArrayList<CalItem>();
@@ -62,18 +65,12 @@ public LocalDateTime s;
         }
 
         adapter.add(newCal);*/
-        return view;
-    }
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState)
-    {
-
         dateview = view.findViewById(R.id.datetemp);
         _context = this.getContext();
-                super.onViewCreated(view, savedInstanceState);
+        super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Class Schedule");
 
-        openDatePickerButton  = view.findViewById(R.id.btnitem);
+        openDatePickerButton = view.findViewById(R.id.btnitem);
         openDatePickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,27 +81,25 @@ public LocalDateTime s;
                 int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
                 DatePickerDialog datePickerDialog = new DatePickerDialog(_context, new DatePickerDialog.OnDateSetListener() {
-                   @Override
-                   public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         dateview.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
                         adapter.clear();
-                       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                           for (int i = 0; i < sss.size(); i++) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            for (int i = 0; i < sss.size(); i++) {
 
-                               if (sss.get(i).ClassDateTime.getDayOfMonth() == dayOfMonth)
-                               {
-                                   adapter.add(sss.get(i));
-                               }
-                           }
-                           if (dayOfMonth == LocalDateTime.now().getDayOfMonth() )
-                           {
+                                if (sss.get(i).ClassDateTime.getDayOfMonth() == dayOfMonth) {
+                                    adapter.add(sss.get(i));
+                                }
+                            }
+                            if (dayOfMonth == LocalDateTime.now().getDayOfMonth()) {
 
 
-                           }
-                       }
+                            }
+                        }
 
-                   }
-               },year,month,dayOfMonth);
+                    }
+                }, year, month, dayOfMonth);
                 datePickerDialog.show();
             }
         });
